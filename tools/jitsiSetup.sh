10745f9d3475d236443bb7af30a4242e2fbbfeca
titus.moldovan <titus.moldovan@8x8.com>
2021-05-04 16:10:35 +0300

    fixes script

--- tools/jitsiSetup.sh
+++ tools/jitsiSetup.sh
@@ -8,7 +8,5 @@ cd output/build_webrtc/webrtc/android/src/
 git remote add titusOrigin https://github.com/tmoldovan8x8/webrtc.git
 git fetch titusOrigin
 git checkout titusOrigin/feature/e2ee_add_encryptor_decryptor
-cd ../../../../.. 
-
-python build-webrtc.py --sync --android output
+cd ../../../../..
 
